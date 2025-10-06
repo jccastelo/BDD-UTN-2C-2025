56240c61b061b7b7372951b8ad4677c59830ec74
jccastelo <jcastelo@frba.utn.edu.ar>
2025-10-06 03:17:31 -0300

    termine la guia listo q capo que soy

--- SQLGuia.sql
+++ SQLGuia.sql
@@ -521,3 +521,286 @@ WHERE p.prod_familia = (SELECT TOP 1 p2.prod_familia
                             ORDER BY SUM(i2.item_cantidad) DESC)
 GROUP BY YEAR(f.fact_fecha), p.prod_familia
 ORDER BY SUM(i.item_cantidad) DESC, p.prod_familia DESC
+
+
+
+-- EJERCICIO 26
+SELECT e.empl_codigo,
+        
+        COUNT(DISTINCT d.depo_codigo) AS depositos_a_cargo,
+
+        ISNULL((
+            SELECT SUM(f2.fact_total) 
+            FROM Factura f2
+            WHERE f2.fact_vendedor = e.empl_codigo AND YEAR(f2.fact_fecha) = 2012
+        ), 0) AS monto_total_facturado,
+
+        (SELECT TOP 1 f3.fact_cliente
+        FROM Factura f3
+        WHERE f3.fact_vendedor = e.empl_codigo AND YEAR(f3.fact_fecha) = 2012
+        GROUP BY f3.fact_cliente
+        ORDER BY COUNT(f3.fact_cliente) DESC) AS cliente_que_mas_vendio,
+
+        (SELECT TOP 1 i.item_producto
+        FROM Item_Factura i 
+        INNER JOIN Factura f4 ON f4.fact_numero = i.item_numero
+                                AND f4.fact_sucursal = i.item_sucursal
+                                AND f4.fact_tipo = i.item_tipo
+        WHERE f4.fact_vendedor = e.empl_codigo AND YEAR(f4.fact_fecha) = 2012
+        GROUP BY i.item_producto
+        ORDER BY SUM(i.item_cantidad) DESC) AS prod_mas_vendido,
+
+        ISNULL(((SELECT SUM(f5.fact_total)
+        FROM Factura f5
+        WHERE f5.fact_vendedor = e.empl_codigo AND YEAR(f5.fact_fecha) = 2012) * 100
+        /
+        (SELECT SUM(f6.fact_total)
+        FROM Factura f6
+        WHERE YEAR(f6.fact_fecha) = 2012)), 0) AS porcentaje_venta
+
+FROM Empleado e
+LEFT OUTER JOIN Deposito d ON d.depo_encargado = e.empl_codigo
+GROUP BY e.empl_codigo
+ORDER BY 3 DESC
+
+
+-- EJERCICIO 27
+SELECT YEAR(f.fact_fecha) AS año,
+        e.enva_codigo,
+        e.enva_detalle,
+        COUNT(DISTINCT p.prod_codigo) AS cant_prod,
+        SUM(i.item_cantidad) AS cant_prod_facturados,
+
+        (SELECT TOP 1 p2.prod_codigo
+        FROM Producto p2
+        INNER JOIN Item_Factura i2 ON i2.item_producto = p2.prod_codigo
+        INNER JOIN Factura f2 ON i2.item_numero = f2.fact_numero
+                            AND i2.item_sucursal = f2.fact_sucursal
+                            AND i2.item_tipo = f2.fact_tipo
+        WHERE p2.prod_envase = e.enva_codigo AND YEAR(f2.fact_fecha) = YEAR(f.fact_fecha)
+        GROUP BY p2.prod_codigo
+        ORDER BY SUM(i2.item_cantidad) DESC
+        ) AS prod_mas_vendido,
+
+        SUM(i.item_cantidad * i.item_precio) AS monto_total_venta,
+
+        SUM(i.item_cantidad * i.item_precio) * 100
+        / (SELECT SUM(f3.fact_total)
+        FROM Factura f3
+        WHERE YEAR(f3.fact_fecha) = YEAR(f.fact_fecha)
+        ) AS porcentaje_venta_envase
+
+FROM Factura f
+INNER JOIN Item_Factura i ON i.item_numero = f.fact_numero
+                            AND i.item_sucursal = f.fact_sucursal
+                            AND i.item_tipo = f.fact_tipo
+INNER JOIN Producto p ON p.prod_codigo = i.item_producto
+INNER JOIN Envases e ON e.enva_codigo = p.prod_envase
+GROUP BY YEAR(f.fact_fecha), e.enva_codigo, e.enva_detalle
+ORDER BY 1 ASC, 5 DESC
+
+
+-- EJERCICIO 28
+SELECT YEAR(f.fact_fecha) AS año,
+        e.empl_codigo AS cod_vendedor,
+        CONCAT(RTRIM(e.empl_nombre), ',', SPACE(1), RTRIM(e.empl_apellido)) AS vendedor_detalle,
+        COUNT(DISTINCT f.fact_numero) AS cant_facturas,
+        COUNT(DISTINCT f.fact_cliente) AS cant_clientes,
+
+        ISNULL((SELECT SUM(i.item_cantidad)
+        FROM Item_Factura i
+        INNER JOIN Composicion c ON i.item_producto = c.comp_producto
+        INNER JOIN Factura f2 ON i.item_numero = f2.fact_numero
+                            AND i.item_sucursal = f2.fact_sucursal
+                            AND i.item_tipo = f2.fact_tipo 
+        WHERE f2.fact_vendedor = e.empl_codigo AND YEAR(f2.fact_fecha) = YEAR(f.fact_fecha)
+        ), 0) AS cant_prod_composicion,
+
+        ISNULL((SELECT SUM(i2.item_cantidad)
+        FROM Item_Factura i2
+        INNER JOIN Factura f3 ON i2.item_numero = f3.fact_numero
+                            AND i2.item_sucursal = f3.fact_sucursal
+                            AND i2.item_tipo = f3.fact_tipo 
+        WHERE f3.fact_vendedor = e.empl_codigo AND YEAR(f3.fact_fecha) = YEAR(f.fact_fecha)
+        AND i2.item_producto NOT IN (SELECT comp_producto FROM Composicion)
+        ), 0) AS cant_prod_no_composicion,
+
+        SUM(f.fact_total) AS monto_total_vendido
+
+FROM Factura f 
+INNER JOIN Empleado e ON f.fact_vendedor = e.empl_codigo
+INNER JOIN Item_Factura i4 ON i4.item_numero = f.fact_numero
+                                AND i4.item_sucursal = f.fact_sucursal
+                                AND i4.item_tipo = f.fact_tipo
+GROUP BY YEAR(f.fact_fecha), e.empl_codigo, e.empl_nombre, e.empl_apellido
+ORDER BY año ASC, COUNT(DISTINCT I4.item_producto) DESC
+
+
+-- EJERCICIO 29
+SELECT p.prod_codigo, p.prod_detalle, p.prod_familia,
+        SUM(i.item_cantidad) AS cantidad_vendida,
+        COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) AS cant_facturas,
+        SUM(i.item_cantidad * i.item_producto) AS monto_total_facturado
+FROM Producto p 
+INNER JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+INNER JOIN Factura f ON i.item_numero = f.fact_numero
+                            AND i.item_sucursal = f.fact_sucursal
+                            AND i.item_tipo = f.fact_tipo
+WHERE prod_familia IN (SELECT p2.prod_familia 
+                        FROM Producto p2
+                        INNER JOIN Familia fa ON fa.fami_id = p2.prod_familia
+                        GROUP BY p2.prod_familia
+                        HAVING COUNT(DISTINCT p2.prod_codigo) > 20)
+AND YEAR(f.fact_fecha) = 2011
+GROUP BY p.prod_codigo, p.prod_detalle, p.prod_familia
+ORDER BY cantidad_vendida DESC
+
+
+-- EJERCICIO 30
+SELECT j.empl_codigo AS jefe_codigo, 
+        CONCAT(RTRIM(j.empl_apellido), ',', SPACE(1), RTRIM(j.empl_nombre)) AS nombre_jefe,
+        COUNT(DISTINCT e.empl_codigo) AS cant_empleados,
+        ISNULL(SUM(f.fact_total), 0) AS monto_vendido_empleados,
+        COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) AS cant_facturas_empleados,
+        (SELECT TOP 1 CONCAT(RTRIM(e2.empl_apellido), ',', SPACE(1), RTRIM(e2.empl_nombre))
+        FROM Factura f2
+        INNER JOIN Empleado e2 ON f2.fact_vendedor = e2.empl_codigo
+        WHERE e2.empl_jefe = j.empl_codigo AND YEAR(f2.fact_fecha) = 2012
+        GROUP BY e2.empl_codigo, e2.empl_apellido, e2.empl_nombre
+        ORDER BY SUM(f2.fact_total) DESC) AS mejor_empleado
+FROM Empleado j
+INNER JOIN Empleado e ON j.empl_codigo = e.empl_jefe
+LEFT OUTER JOIN Factura f ON f.fact_vendedor = e.empl_codigo
+WHERE YEAR(f.fact_fecha) = 2012
+GROUP BY j.empl_codigo, j.empl_nombre, j.empl_apellido
+HAVING COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) > 10
+ORDER BY monto_vendido_empleados DESC
+
+
+-- EJERCICIO 32
+SELECT f1.fami_id,
+        f1.fami_detalle,
+        f2.fami_id,
+        f2.fami_detalle,
+        COUNT(DISTINCT i1.item_numero + i1.item_sucursal + i2.item_tipo) AS cant_facturas,
+        (SUM(i1.item_cantidad * i1.item_precio) + SUM(i2.item_cantidad * i2.item_precio)) AS total_vendido
+FROM Item_Factura i1
+INNER JOIN Item_Factura i2 ON i1.item_numero + i1.item_sucursal + i1.item_tipo = i2.item_numero + i2.item_sucursal + i2.item_tipo
+INNER JOIN Producto p1 ON p1.prod_codigo = i1.item_producto
+INNER JOIN Producto p2 ON p2.prod_codigo = i2.item_producto
+INNER JOIN Familia f1 ON p1.prod_familia = f1.fami_id
+INNER JOIN Familia f2 ON p2.prod_familia = f2.fami_id
+WHERE f1.fami_id < f2.fami_id
+GROUP BY f1.fami_id, f1.fami_detalle, f2.fami_id, f2.fami_detalle
+HAVING COUNT(DISTINCT i1.item_numero + i1.item_sucursal + i2.item_tipo) > 10
+ORDER BY total_vendido DESC
+
+
+-- EJERCICIO 33
+SELECT p.prod_codigo,
+        p.prod_detalle,
+        SUM(i.item_cantidad) AS cant_vendidas,
+        COUNT(DISTINCT i.item_numero + i.item_sucursal + i.item_tipo) AS cant_facturas,
+        AVG(i.item_precio) AS precio_promedio,
+        SUM(i.item_cantidad * i.item_precio) AS total_facturado
+FROM Producto p 
+INNER JOIN Composicion c ON p.prod_codigo = c.comp_componente
+INNER JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+INNER JOIN Factura f ON i.item_numero = f.fact_numero
+                            AND i.item_sucursal = f.fact_sucursal
+                            AND i.item_tipo = f.fact_tipo
+WHERE YEAR(f.fact_fecha) = 2012 
+AND c.comp_producto = (SELECT TOP 1 c2.comp_producto
+                    FROM Composicion c2
+                    INNER JOIN Item_Factura i2 ON i2.item_producto = c2.comp_producto
+                    INNER JOIN Factura f2 ON i2.item_numero = f2.fact_numero
+                            AND i2.item_sucursal = f2.fact_sucursal
+                            AND i2.item_tipo = f2.fact_tipo
+                    WHERE YEAR(f2.fact_fecha) = 2012
+                    GROUP BY c2.comp_producto
+                    ORDER BY SUM(i2.item_cantidad) DESC)
+GROUP BY p.prod_codigo, p.prod_detalle
+ORDER BY total_facturado DESC
+
+
+-- EJERCICIO 34
+SELECT r.rubr_id,
+        MONTH(f.fact_fecha) AS mes,
+        COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) AS cant_facturas_mal_hechas
+FROM Rubro r 
+LEFT OUTER JOIN Producto p ON p.prod_rubro = r.rubr_id
+LEFT OUTER JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+LEFT OUTER JOIN Factura f ON i.item_numero = f.fact_numero
+                            AND i.item_sucursal = f.fact_sucursal
+                            AND i.item_tipo = f.fact_tipo
+WHERE YEAR(f.fact_fecha) = 2011
+AND (f.fact_numero + f.fact_sucursal + f.fact_tipo) IN (SELECT i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        FROM Item_Factura i2
+                                                        INNER JOIN Item_Factura i3 ON i3.item_numero + i3.item_sucursal + i3.item_tipo = i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        INNER JOIN Producto p2 ON p2.prod_codigo = i2.item_producto
+                                                        INNER JOIN Producto p3 ON p3.prod_codigo = i3.item_producto
+                                                        WHERE p2.prod_rubro <> p3.prod_rubro AND i2.item_producto <> i3.item_producto
+                                                        GROUP BY i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        )
+GROUP BY r.rubr_id, MONTH(f.fact_fecha)
+ORDER BY 2 ASC, 1 ASC
+
+-- otra solucion, mejor?
+SELECT r.rubr_id,
+    Meses.mes as Mes,
+    COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) AS cant_facturas_mal_hechas
+FROM Rubro r 
+CROSS JOIN (SELECT 1 as Mes
+UNION SELECT 2 AS Mes
+UNION SELECT 3 AS Mes
+UNION SELECT 4 AS Mes
+UNION SELECT 5 AS Mes
+UNION SELECT 6 AS Mes
+UNION SELECT 7 AS Mes
+UNION SELECT 8 AS Mes
+UNION SELECT 9 AS Mes
+UNION SELECT 10 AS Mes
+UNION SELECT 11 AS Mes
+UNION SELECT 12 AS Mes) as Meses
+LEFT OUTER JOIN Producto p ON p.prod_rubro = r.rubr_id
+LEFT OUTER JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+LEFT OUTER JOIN Factura f 
+ON i.item_numero = f.fact_numero
+AND i.item_sucursal = f.fact_sucursal
+AND i.item_tipo = f.fact_tipo
+AND YEAR(f.fact_fecha) = 2011
+AND MONTH(f.fact_fecha) = Meses.Mes
+AND (f.fact_numero + f.fact_sucursal + f.fact_tipo) IN (SELECT i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        FROM Item_Factura i2
+                                                        INNER JOIN Item_Factura i3 ON i3.item_numero + i3.item_sucursal + i3.item_tipo = i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        INNER JOIN Producto p2 ON p2.prod_codigo = i2.item_producto
+                                                        INNER JOIN Producto p3 ON p3.prod_codigo = i3.item_producto
+                                                        WHERE p2.prod_rubro <> p3.prod_rubro AND i2.item_producto <> i3.item_producto
+                                                        GROUP BY i2.item_numero + i2.item_sucursal + i2.item_tipo
+                                                        )
+GROUP BY r.rubr_id, Meses.Mes
+ORDER BY Meses.Mes ASC, r.rubr_id ASC
+
+
+-- EJERCICIO 35
+SELECT YEAR(f.fact_fecha) AS año,
+        p.prod_codigo,
+        p.prod_detalle,
+        COUNT(DISTINCT f.fact_numero + f.fact_sucursal + f.fact_tipo) AS cant_facturas,
+        COUNT(DISTINCT f.fact_cliente) AS cant_clientes,
+        COUNT(DISTINCT c.comp_componente) AS cant_componentes,
+        ((SUM(i.item_cantidad * i.item_precio) * 100 )
+        / (SELECT SUM(i2.item_cantidad * i2.item_precio)
+            FROM Factura f2 
+            INNER JOIN Item_Factura i2 ON i2.item_numero = f2.fact_numero
+                            AND i2.item_sucursal = f2.fact_sucursal
+                            AND i2.item_tipo = f2.fact_tipo
+            WHERE YEAR(f2.fact_fecha) = YEAR(f.fact_fecha))) AS porcentaje_sobre_total
+FROM Producto p 
+INNER JOIN Item_Factura i ON i.item_producto = p.prod_codigo
+INNER JOIN Factura f ON i.item_numero = f.fact_numero
+                            AND i.item_sucursal = f.fact_sucursal
+                            AND i.item_tipo = f.fact_tipo
+LEFT OUTER JOIN Composicion c ON p.prod_codigo = c.comp_producto
+GROUP BY YEAR(f.fact_fecha), p.prod_codigo, p.prod_detalle
+ORDER BY año ASC, SUM(i.item_cantidad) DESC

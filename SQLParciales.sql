b25d1a7f1e728fd3d7c6830c9d655a1891416b83
jccastelo <jcastelo@frba.utn.edu.ar>
2025-10-06 17:39:44 -0300

    parcial 24/6/25

--- SQLParciales.sql
+++ SQLParciales.sql
@@ -52,3 +52,87 @@ AND i.item_producto IN ((SELECT TOP 10 imas.item_producto
                         )
 GROUP BY c.clie_codigo, c.clie_razon_social
 ORDER BY 1 ASC
+
+
+
+-- 1C2025 24/06
+SELECT YEAR(f.fact_fecha) AS año,
+        fa.fami_id,
+
+        fa.fami_detalle,
+
+        (SELECT TOP 1 c1.clie_razon_social
+        FROM Factura f1
+        INNER JOIN Cliente c1 ON f1.fact_cliente = c1.clie_codigo
+        INNER JOIN Item_Factura i1 ON f1.fact_numero = i1.item_numero
+                            AND f1.fact_sucursal = i1.item_sucursal
+                            AND f1.fact_tipo = i1.item_tipo
+        INNER JOIN Producto p1 ON p1.prod_codigo = i1.item_producto
+        INNER JOIN Familia fa1 ON p1.prod_familia = fa1.fami_id
+        WHERE fa1.fami_id = fa.fami_id AND YEAR(f1.fact_fecha) = YEAR(f.fact_fecha)
+        GROUP BY f1.fact_cliente, c1.clie_razon_social
+        ORDER BY COUNT(DISTINCT i1.item_producto) ASC
+        ) AS clie_razon_social,
+
+        (SELECT TOP 1 SUM(i2.item_cantidad)
+        FROM Factura f2
+        INNER JOIN Item_Factura i2 ON f2.fact_numero = i2.item_numero
+                            AND f2.fact_sucursal = i2.item_sucursal
+                            AND f2.fact_tipo = i2.item_tipo
+        INNER JOIN Producto p2 ON p2.prod_codigo = i2.item_producto
+        INNER JOIN Familia fa2 ON p2.prod_familia = fa2.fami_id
+        WHERE fa2.fami_id = fa.fami_id AND YEAR(f2.fact_fecha) = YEAR(f.fact_fecha)
+        GROUP BY f2.fact_cliente
+        ORDER BY COUNT(DISTINCT i2.item_producto) ASC 
+        ) AS cant_unidades_compradas,
+
+        COUNT(DISTINCT p.prod_codigo) AS productos_familia
+
+FROM Factura f 
+INNER JOIN Item_Factura i ON f.fact_numero = i.item_numero
+                            AND f.fact_sucursal = i.item_sucursal
+                            AND f.fact_tipo = i.item_tipo
+INNER JOIN Producto p ON i.item_producto = p.prod_codigo
+INNER JOIN Familia fa ON fa.fami_id = p.prod_familia
+GROUP BY YEAR(f.fact_fecha), fa.fami_id, fa.fami_detalle
+UNION ALL
+SELECT YEAR(f.fact_fecha) AS año,
+        fa.fami_id,
+
+        fa.fami_detalle,
+
+        (SELECT TOP 1 c1.clie_razon_social
+        FROM Factura f1
+        INNER JOIN Cliente c1 ON f1.fact_cliente = c1.clie_codigo
+        INNER JOIN Item_Factura i1 ON f1.fact_numero = i1.item_numero
+                            AND f1.fact_sucursal = i1.item_sucursal
+                            AND f1.fact_tipo = i1.item_tipo
+        INNER JOIN Producto p1 ON p1.prod_codigo = i1.item_producto
+        INNER JOIN Familia fa1 ON p1.prod_familia = fa1.fami_id
+        WHERE fa1.fami_id = fa.fami_id AND YEAR(f1.fact_fecha) = YEAR(f.fact_fecha)
+        GROUP BY f1.fact_cliente, c1.clie_razon_social
+        ORDER BY SUM(i1.item_cantidad * i1.item_precio) DESC
+        ) AS clie_razon_social,
+
+        (SELECT TOP 1 SUM(i2.item_cantidad)
+        FROM Factura f2
+        INNER JOIN Item_Factura i2 ON f2.fact_numero = i2.item_numero
+                            AND f2.fact_sucursal = i2.item_sucursal
+                            AND f2.fact_tipo = i2.item_tipo
+        INNER JOIN Producto p2 ON p2.prod_codigo = i2.item_producto
+        INNER JOIN Familia fa2 ON p2.prod_familia = fa2.fami_id
+        WHERE fa2.fami_id = fa.fami_id AND YEAR(f2.fact_fecha) = YEAR(f.fact_fecha)
+        GROUP BY f2.fact_cliente
+        ORDER BY SUM(i2.item_cantidad * i2.item_precio) DESC
+        ) AS cant_unidades_compradas,
+
+        COUNT(DISTINCT p.prod_codigo) AS productos_familia
+
+FROM Factura f 
+INNER JOIN Item_Factura i ON f.fact_numero = i.item_numero
+                            AND f.fact_sucursal = i.item_sucursal
+                            AND f.fact_tipo = i.item_tipo
+INNER JOIN Producto p ON i.item_producto = p.prod_codigo
+INNER JOIN Familia fa ON fa.fami_id = p.prod_familia
+GROUP BY YEAR(f.fact_fecha), fa.fami_id, fa.fami_detalle
+ORDER BY año ASC, productos_familia ASC, fa.fami_id ASC

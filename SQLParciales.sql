8156ddf834bb2c6b17dcffbe888a8ac8ae3047fe
jccastelo <jcastelo@frba.utn.edu.ar>
2025-10-07 12:11:33 -0300

    te acordas q no paraba de hacer parciales

--- SQLParciales.sql
+++ SQLParciales.sql
@@ -136,3 +136,70 @@ INNER JOIN Producto p ON i.item_producto = p.prod_codigo
 INNER JOIN Familia fa ON fa.fami_id = p.prod_familia
 GROUP BY YEAR(f.fact_fecha), fa.fami_id, fa.fami_detalle
 ORDER BY año ASC, productos_familia ASC, fa.fami_id ASC
+
+
+
+
+-- 1C2025 24/06
+SELECT TOP 5 CONCAT(RTRIM(e.empl_apellido),',',SPACE(1),RTRIM(e.empl_nombre)) AS apellido_y_nombre,
+        SUM(i.item_cantidad) AS total_unidades_vendidas,
+        AVG(f.fact_total) AS promedio_por_factura,
+        SUM(f.fact_total) AS monto_total_ventas
+FROM Empleado e 
+INNER JOIN Factura f ON f.fact_vendedor = e.empl_codigo
+INNER JOIN Item_Factura i ON f.fact_numero = i.item_numero
+                        AND f.fact_sucursal = i.item_sucursal
+                        AND f.fact_tipo = i.item_tipo
+--WHERE YEAR(f.fact_fecha) = DATEADD(YEAR, -1, GETDATE())
+WHERE YEAR(f.fact_fecha) = 2012
+AND (i.item_numero + i.item_tipo + i.item_sucursal) IN (SELECT i2.item_numero + i2.item_tipo + i2.item_sucursal
+                                                        FROM Item_Factura i2 
+                                                        GROUP BY i2.item_numero, i2.item_tipo, i2.item_sucursal
+                                                        HAVING (COUNT(*)) > 2)
+GROUP BY e.empl_codigo, e.empl_apellido, e.empl_nombre
+ORDER BY COUNT(DISTINCT f.fact_cliente) ASC, monto_total_ventas DESC, e.empl_codigo ASC
+
+
+
+-- ns otro parcial
+--zonas con 2 depositos
+SELECT z.zona_detalle,
+        COUNT(DISTINCT d.depo_codigo) AS cant_depositos,
+        COUNT(DISTINCT s.stoc_producto) AS cant_prod_distintos,
+        COUNT(DISTINCT i.item_producto) AS cant_prod_distintos_vendidos
+FROM Zona z 
+INNER JOIN Deposito d ON d.depo_zona = z.zona_codigo
+LEFT OUTER JOIN Stock s ON s.stoc_deposito = d.depo_codigo
+LEFT OUTER JOIN Item_Factura i ON i.item_producto = s.stoc_producto
+GROUP BY z.zona_codigo, z.zona_detalle
+HAVING COUNT(DISTINCT d.depo_codigo) >= 2
+ORDER BY (SELECT COUNT(e.empl_codigo) 
+        FROM Empleado e
+        INNER JOIN Departamento de ON e.empl_departamento = de.depa_codigo
+        WHERE de.depa_zona = z.zona_codigo) DESC
+
+
+
+--RECU 1C25 1/7
+SELECT CONCAT(RTRIM(e.empl_apellido),',',SPACE(1),RTRIM(e.empl_nombre)) AS apellido_y_nombre,
+        'Mejor Facturación' AS ranking
+FROM Empleado e 
+INNER JOIN Factura f ON e.empl_codigo = f.fact_vendedor
+WHERE e.empl_codigo = (SELECT TOP 1 f2.fact_vendedor
+                        FROM Factura f2 
+                        WHERE YEAR(f2.fact_fecha) = (SELECT MAX(YEAR(f2.fact_fecha)) FROM Factura f2)
+                        GROUP BY f2.fact_vendedor
+                        ORDER BY SUM(f2.fact_total) DESC)
+GROUP BY e.empl_codigo, e.empl_apellido, e.empl_nombre
+UNION ALL
+SELECT CONCAT(RTRIM(e.empl_apellido),',',SPACE(1),RTRIM(e.empl_nombre)) AS apellido_y_nombre,
+        'Vendio Más Facturas' AS ranking
+FROM Empleado e 
+INNER JOIN Factura f ON e.empl_codigo = f.fact_vendedor
+WHERE e.empl_codigo = (SELECT TOP 1 f2.fact_vendedor
+                        FROM Factura f2 
+                        WHERE YEAR(f2.fact_fecha) = (SELECT MAX(YEAR(f2.fact_fecha)) FROM Factura f2)
+                        GROUP BY f2.fact_vendedor
+                        ORDER BY COUNT(DISTINCT f2.fact_numero + f2.fact_sucursal + f2.fact_tipo) DESC)
+GROUP BY e.empl_codigo, e.empl_apellido, e.empl_nombre
+

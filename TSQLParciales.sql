a9f0605df1110bea7fcb37bc901085a8616a8196
jccastelo <jcastelo@frba.utn.edu.ar>
2025-11-11 01:05:56 -0300

    otro parcialongo

--- TSQLParciales.sql
+++ TSQLParciales.sql
@@ -61,3 +61,103 @@ INNER JOIN Item_factura_precio ip ON ip.item_precio_numero = i.item_numero
                                 AND ip.item_precio_producto = i.item_producto
 GROUP BY i.item_producto, i.item_numero, i.item_sucursal, i.item_tipo
 GO
+
+
+
+
+-- 2024 2C
+
+CREATE TABLE auditoria_cliente (
+    clie_codigo CHAR(6),
+    clie_razon_social CHAR(100),
+    clie_telefono CHAR(100),
+    clie_domicilio CHAR(100),
+    clie_limite_credito DECIMAL(12,2),
+    clie_vendedor NUMERIC(6),
+    tipo_operacion CHAR(10),
+    fecha_ejecucion DATETIME2,
+    intento_masiva BIT
+)
+GO
+
+CREATE TRIGGER TR_AUDITAR_CLIENTE
+ON Cliente
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+
+    DECLARE @clie_codigo CHAR(6),
+            @clie_razon_social CHAR(100),
+            @clie_telefono CHAR(100),
+            @clie_domicilio CHAR(100),
+            @clie_limite_credito DECIMAL(12,2),
+            @clie_vendedor NUMERIC(6),
+            @tipo_operacion CHAR(10),
+            @fecha_ejecucion DATETIME2,
+            @intento_masiva BIT
+
+    SET @fecha_ejecucion = SYSDATETIME()
+
+    IF EXISTS (SELECT 1 FROM inserted) AND NOT EXISTS (SELECT 1 FROM deleted)
+        SET @tipo_operacion = 'INSERT'
+    ELSE IF EXISTS (SELECT 1 FROM deleted) AND NOT EXISTS (SELECT 1 FROM inserted)
+        SET @tipo_operacion = 'DELETE'
+    ELSE
+        SET @tipo_operacion = 'UPDATE'
+        
+    IF (SELECT COUNT(*) FROM inserted) > 1 OR (SELECT COUNT(*) FROM deleted) > 1
+    BEGIN
+
+        SET @intento_masiva = 1
+
+        INSERT INTO auditoria_cliente (tipo_operacion, fecha_ejecucion, intento_masiva)
+        VALUES(@tipo_operacion, @fecha_ejecucion, @intento_masiva)
+
+        ROLLBACK TRANSACTION;
+        RETURN;
+
+    END;
+
+    SET @intento_masiva = 0
+
+    IF @tipo_operacion = 'INSERT' OR @tipo_operacion = 'DELETE'
+    BEGIN
+
+        SELECT @clie_codigo = i.clie_codigo,
+                @clie_razon_social = i.clie_razon_social,
+                @clie_telefono = i.clie_telefono,
+                @clie_domicilio = i.clie_domicilio,
+                @clie_limite_credito = i.clie_limite_credito,
+                @clie_vendedor = i.clie_vendedor
+        FROM inserted i
+
+        INSERT INTO auditoria_cliente (clie_codigo, clie_razon_social, clie_telefono, clie_domicilio,
+                                        clie_limite_credito, clie_vendedor, tipo_operacion, fecha_ejecucion,
+                                        intento_masiva)
+        VALUES(@clie_codigo, @clie_razon_social, @clie_telefono, @clie_domicilio,
+                @clie_limite_credito, @clie_vendedor, @tipo_operacion, @fecha_ejecucion,
+                @intento_masiva)
+
+    END;
+    ELSE
+    BEGIN
+
+        SELECT @clie_codigo = i.clie_codigo,
+                @clie_razon_social = i.clie_razon_social,
+                @clie_telefono = i.clie_telefono,
+                @clie_domicilio = i.clie_domicilio,
+                @clie_limite_credito = i.clie_limite_credito,
+                @clie_vendedor = i.clie_vendedor
+        FROM deleted i
+
+        INSERT INTO auditoria_cliente (clie_codigo, clie_razon_social, clie_telefono, clie_domicilio,
+                                        clie_limite_credito, clie_vendedor, tipo_operacion, fecha_ejecucion,
+                                        intento_masiva)
+        VALUES(@clie_codigo, @clie_razon_social, @clie_telefono, @clie_domicilio,
+                @clie_limite_credito, @clie_vendedor, @tipo_operacion, @fecha_ejecucion,
+                @intento_masiva)
+
+    END;
+
+END;
+GO
